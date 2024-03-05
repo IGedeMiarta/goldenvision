5cbbdb5f37dd483150c631474dc0b6698d403d7a
Risfandi Bayu <risfandibayu19@gmail.com>
2024-03-05 22:14:41 +0700

    update

--- core/app/Http/Helpers/helpers.php
+++ core/app/Http/Helpers/helpers.php
@@ -1141,8 +1141,8 @@ function  leaderCommission($id, $qty)
     while ($id != "" || $id != "0") {
         if (isUserExists($id)) {
             $refid = getRefId($id);
-            $user = user::where('id',$id)->first();
-            $userRef = user::where('id',$refid)->first();
+            $user = user::find($id);
+            $userRef = user::find($refid);
             if ($userRef->rank == 0) {
                 $id = $refid;
                 continue;
