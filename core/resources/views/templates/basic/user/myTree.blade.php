c532b33c8e5a13b58e4b2c8014c4a6db15af4ccd
I Gede Miarta Yasa <miartayasa10@gmail.com>
2024-03-07 21:26:41 +0800

    update info wd

--- core/resources/views/templates/basic/user/myTree.blade.php
+++ core/resources/views/templates/basic/user/myTree.blade.php
@@ -90,9 +90,9 @@
                     </div>
                     <div style="display: block; text-align: start;" class="mb-3">
                         <h4 style="color: #000; font-size: 18px;">Carry Forward Binary Point</h4>
-                        <span style="color: #000;">Left {{ auth()->user()->paid_left - auth()->user()->bv_left }}</span>
+                        <span style="color: #000;">Left {{ auth()->user()->paid_left }}</span>
                         <br>
-                        <span style="color: #000;">Right {{ auth()->user()->paid_right - auth()->user()->bv_right }}</span>
+                        <span style="color: #000;">Right {{ auth()->user()->paid_right }}</span>
                     </div>
                 </div>
             </div>
