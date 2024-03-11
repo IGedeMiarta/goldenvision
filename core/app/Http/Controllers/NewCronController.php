002629d502d46ad8d91ada41953ec7dde8d36b5b
Risfandi Bayu <risfandibayu19@gmail.com>
2024-03-12 03:49:10 +0700

    update jam cron

--- core/app/Http/Controllers/NewCronController.php
+++ core/app/Http/Controllers/NewCronController.php
@@ -33,7 +33,7 @@ class NewCronController extends Controller
 
             $us = User::where('id', $uex->user_id)->first();
 
-            if (Date('H') != "00") {
+            if (Date('H') != "03") {
                 continue;
             }
 
